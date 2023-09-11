cea545dcd187f9c8817c8d902c93c69a0aad7f25
aZ <94156430+61-5a@users.noreply.github.com>
2023-09-11 14:01:41 +0530

    update image upload feild

--- src/components/addOrEditUsers.tsx
+++ src/components/addOrEditUsers.tsx
@@ -151,37 +151,73 @@ const AddOrEditUsers: React.FC<AddOrEditUsersProps> = ({ editUser, setEditUser, 
             Profile Image Upload
           </label>
           <div className="flex items-center justify-center w-full">
-            {formData.photo ? <img src={formData.photo} className={styles.uploded_img} alt="image" /> : null}
-            <label className="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
-              <div className="flex flex-col items-center justify-center">
-                <div className="flex flex-col items-center justify-center pt-7">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                    />
-                  </svg>
-                  <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">Attach a file</p>
+            {formData.photo ? (
+              <>
+                <div className="flex flex-col w-full h-32">
+                  <div className="flex flex-col items-center justify-left z-2">
+                    <div className={styles.uploded_img_outer}>
+                      <img
+                        src={formData.photo}
+                        className={styles.uploded_img}
+                        alt="image"
+                        onClick={() => {
+                          const photoInput = document.getElementById("photoInput") as HTMLInputElement | null;
+                          if (photoInput) {
+                            photoInput.click();
+                          }
+                        }}
+                      />
+                      <img
+                        onClick={() => setFormData({ ...formData, photo: "" })}
+                        src="/img/icons/close.svg"
+                        className={styles.uploded_close_img}
+                        alt="Close"
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <input
-                type="file"
-                id="photo"
-                name="photo"
-                accept="image/*"
-                onChange={handleFileInputChange}
-                required={!editUser}
-                className="opacity-0"
-              />
-            </label>
+                <input
+                  type="file"
+                  id="photoInput"
+                  name="photo"
+                  accept="image/*"
+                  onChange={handleFileInputChange}
+                  required={!editUser}
+                  className="opacity-0 hidden"
+                />
+              </>
+            ) : (
+              <label className="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
+                <div className="flex flex-col items-center justify-center">
+                  <div className="flex flex-col items-center justify-center pt-7">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                      />
+                    </svg>
+                    <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">Attach a file</p>
+                  </div>
+                </div>
+                <input
+                  type="file"
+                  id="photo"
+                  name="photo"
+                  accept="image/*"
+                  onChange={handleFileInputChange}
+                  required={!editUser}
+                  className="opacity-0"
+                />
+              </label>
+            )}
           </div>
         </div>
         <div className={styles.submit}>
